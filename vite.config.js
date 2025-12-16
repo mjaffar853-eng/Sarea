af767f5170c5066c3a2933626e4365bb5f4a0fab
AakashShukla18 <111345414+himanshuvirtualize@users.noreply.github.com>
2025-12-16 15:39:11 +0530

    Updated package.json, vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import restart from 'vite-plugin-restart'
 import tailwindcss from '@tailwindcss/vite'
 
 export default {
-    base: '/Sarea/',
+    base: '/',
     root: 'src/', // Sources files (typically where index.html is)
     publicDir: '../static/', // Path from "root" to static assets (files that are served as they are)
     server:
